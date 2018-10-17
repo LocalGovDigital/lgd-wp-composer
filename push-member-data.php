1d77872dfb24c70bcf703ce6358eda815453928f
Ben <BforBen@users.noreply.github.com>
2018-10-17 22:13:42 +0100

    Update push-member-data.php
     - Expand public sector email checker.
     - Handle Slack response better to check if person has already been invited etc

--- push-member-data.php
+++ push-member-data.php
@@ -18,6 +18,19 @@ function getClient()
     return $client;
 }
 
+function publicSectorEmail ( $email ) {
+    /* Only do this for:
+    - *.gov.uk
+    - *.police.uk
+    - *.nhs.uk
+    - (*.)nhs.net
+    - (*.)gov.je
+    - (*.)gov.scot
+    - (*.)gov.wales
+    */
+    return preg_match( '/^[\w\'\.+\-]+@(?>[\w\.\-]+\.(?>gov|police|nhs)\.uk|(?:[\w\.\-]+\.)?(?>nhs.net|gov.je|gov.scot|gov.wales))$/i', $email );
+}
+
 function inviteToSlack( $email )
 {
     try {
@@ -25,32 +38,29 @@ function inviteToSlack( $email )
         if ( empty( SLACK_TOKEN ) )
             return false;
 
-        /* Only do this for:
-            - *.gov.uk
-            - *.police.uk
-            - *.nhs.uk
-            - (*.)nhs.net
-            - (*.)gov.scot
-            - (*.)gov.wales
-         */
-        if ( !preg_match( '^[\w\.+\-]+@(?>[\w\.\-]+\.(?>gov|police|nhs)\.uk|(?:[\w\.\-]+\.)?(?>nhs.net|gov.scot|gov.wales))$/i', $email ) )
+        if ( !publicSectorEmail( $email ) )
             return false;
 
         $client = new GuzzleHttp\Client([ 'base_uri' => 'https://localgovdigital.slack.com' ]);
 
+        // Based on guidance in https://github.com/ErikKalkoken/slackApiDoc/blob/master/users.admin.invite.md
+        // Invite email address and resend if the invite email was sent a while ago
         $response = $client->request('POST', '/api/users.admin.invite', [
             'form_params' => [
                 'email' => $email,
                 'token' => SLACK_TOKEN,
-                'set_active' => true
+                'set_active' => true,
+                'resend' => true
             ]
         ]);
         
         $body = json_decode( $response->getBody() );
 
-        return $body->{'ok'};
+        // If OK or if already invited etc, return true
+        return ($body->{'ok'} || ( isset( $body->{'error'} ) && in_array( $body->{'error'}, array('already_invited', 'already_in_team', 'sent_recently', 'user_disabled') ) ) );
 
     } catch (Exception $e) {
+        // Any sort of error, return false for further investigation
         return false;
     }
 }
